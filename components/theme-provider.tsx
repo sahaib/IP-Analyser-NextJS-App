6a231376b9335425ca1ad5e049aa4ba162651be4
Sahib Singh <sahaibsingh001@live.com>
2025-01-15 16:33:59 +0530

    fix: update theme provider implementation to fix type errors

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,6 +4,11 @@ import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import type { ThemeProviderProps } from "next-themes"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+  children,
+  ...props
+}: {
+  children: React.ReactNode
+} & ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 } 
